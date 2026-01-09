b40b844a909d6a9ccc0c978d51f196f5f671e5d1
Mauro Vanetti <mauro_vanetti@docenti.naba.it>
2026-01-09 12:04:19 +0100

    Main loop + game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,36 +1,56 @@
 using System.Collections;
 using TMPro;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     public GameObject[] targets;
     public float spawnInterval = 1.0f;
     private int score = 0;
+    private bool isGameActive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {        
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnInterval);
             int index = Random.Range(0, targets.Length);
             Instantiate(targets[index]);
-            UpdateScore(5);
         }
     }
     
-    private void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
-        score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        if (isGameActive)
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private GameManager gameManager;
     private Rigidbody body;
     public float minInitialImpulse;
     public float maxInitialImpulse;
     public float maxInitialTorque;
     public float initialXRange;
     public float initialY;
+    public int pointValue;
+    public ParticleSystem explosion;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         body = GetComponent<Rigidbody>();
         transform.position = new Vector2(RandomInitialX(), initialY);
         body.AddForce(RandomUpwardForce(), 
@@ -41,6 +45,12 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosion, transform.position, explosion.transform.rotation);
+        if (pointValue < 0)
+        {
+            gameManager.GameOver();
+        }
         Destroy(gameObject);
     }
 
